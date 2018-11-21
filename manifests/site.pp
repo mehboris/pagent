b7b5455d5eb820feb36d9f46c2182aede7a88c47
mehboris@gmail.com <mehboris@gmail.com>
2018-11-21 18:12:56 +0200

    first1

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,11 @@ node default {
 package { 'ruby':
 ensure => installed,
 }
-package { 'puppet-lint':
-ensure => installed,
-provider => gem,
-}
+#package { 'puppet-lint':
+#ensure => installed,
+#provider => gem,
+#}
+
 package { 'r10k':
 ensure => installed,
 provider => puppet_gem,
@@ -14,7 +15,7 @@ provider => puppet_gem,
 
 cron { 'puppet-apply':
 ensure  => present,
-command =>' cd /root/depconf5 ; /usr/bin/git pull',
+command =>' cd /etc/puppetlabs/code/environments/production ; /usr/bin/git pull',
 user    => root,
 hour => '*',
 minute => '*/1',
