09d2ee512da86881e8930aea2a8d7c6090fd31d4
mehboris@gmail.com <you@example.com>
2018-11-23 14:45:44 +0000

    upnode2.0

--- upnod.sh
+++ upnod.sh
@@ -8,15 +8,16 @@ cd /etc/puppetlabs/code/environments/
 mv production [production.back
 mkdir production 
 cd production
+ssh-keygen -b 4096 -t rsa -f ~/.ssh/git_serv_rsa -q -N ""
+ssh-copy-id -i ~/.ssh/git_serv_rsa.pub git@$gitserv
+echo Host $gitserv > ~/.ssh/config 
+echo IdentityFile ~/.ssh/git_serv_rsa >>  ~/.ssh/config 
+echo IdentitiesOnly yes  >>  ~/.ssh/config 
 git config --global user.name "mehboris"
 git config --global user.name mehboris@gmail.com
 git init
 git remote add origin git@$gitserv:/home/git/repos/puppetstart.git
-git pull
-ssh-keygen -b 4096 -t rsa -f ~/.ssh/git_serv_rsa -q -N ""
-ssh-copy-id -i ~/.ssh/git_serv_rsa git@$gitserv
-echo Host $gitserv > ~/.ssh/configtest
-echo IdentityFile ~/.ssh/git_serv_rsa >>  ~/.ssh/configtest
-echo IdentitiesOnly yes  >>  ~/.ssh/configtest
-/opt/puppetlabs/bin/puppet apply manifests/site.pp
+git pull origin master
+
+/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
 
