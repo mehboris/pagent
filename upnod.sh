5ec06d09105fb4095fd38f4fdb1e7ebd6a6802a9
mehboris <you@example.com>
2018-11-25 17:09:27 -0500

    some fixes)

--- upnod.sh
+++ upnod.sh
@@ -1,23 +1,27 @@
-gitserv=10.0.2.13
-wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
-sudo dpkg -i puppet5-release-xenial.deb
-sudo apt update
-echo export PATH=/opt/puppetlabs/bin:'$PATH' >> ~/.bashrc 
-apt-get install puppet-agent
+gitserv=10.0.2.12
+sudo rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
+ yum update -y
+ yum install -y nano epel-release && yum repolist
+ systemctl disable firewalld
+
+echo export PATH=/opt/puppetlabs/bin:'$PATH' >> ~/.bashrc
+yum install -y puppet
 cd /etc/puppetlabs/code/environments/
-mv production [production.back
-mkdir production 
+rm -rf production.back
+mv production production.back
+mkdir production
 cd production
 ssh-keygen -b 4096 -t rsa -f ~/.ssh/git_serv_rsa -q -N ""
 ssh-copy-id -i ~/.ssh/git_serv_rsa.pub git@$gitserv
-echo Host $gitserv > ~/.ssh/config 
-echo IdentityFile ~/.ssh/git_serv_rsa >>  ~/.ssh/config 
-echo IdentitiesOnly yes  >>  ~/.ssh/config 
-git config --global user.name "mehboris"
+echo Host $gitserv > ~/.ssh/config
+echo IdentityFile ~/.ssh/git_serv_rsa >>  ~/.ssh/config
+echo IdentitiesOnly yes  >>  ~/.ssh/config
+git config --global user.name "git"
 git config --global user.name mehboris@gmail.com
 git init
 git remote add origin git@$gitserv:/home/git/repos/puppetstart.git
 git pull origin master
 
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
+
 
